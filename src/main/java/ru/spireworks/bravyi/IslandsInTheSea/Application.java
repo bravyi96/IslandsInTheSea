416abb6b33d465d75ceca205b1f6f2b1083b92cb
bravyi96 <maksim.bravyi@yandex.ru>
2020-10-02 22:24:33 +0300

    Rename FileTools to IslandTools Added getAllIslands method in IslandTools class

--- src/main/java/ru/spireworks/bravyi/IslandsInTheSea/Application.java
+++ src/main/java/ru/spireworks/bravyi/IslandsInTheSea/Application.java
@@ -1,15 +1,32 @@
 package ru.spireworks.bravyi.IslandsInTheSea;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.log4j.Logger;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class Application {
+    private static final Logger LOGGER = Logger.getLogger(Application.class);
     private static final String FILE_PATH = "input_data.txt";
+    private IslandTools islandTools;
+
+    private void start() {
+        List<String> data = islandTools.readFile(FILE_PATH);
+        List<Island> islands = islandTools.getAllIslands(data);
+
+        for (Island island : islands) {
+            LOGGER.info("Размер острова: " + island.getSize());
+            LOGGER.info("Координаты по горизонтали: " + island.getHCoords());
+            LOGGER.info("Координаты по вертикали: " + island.getVCoords());
+        }
+    }
 
     public static void main( String[] args ) {
-        List<String> data = FileTools.readFile(FILE_PATH);
-
-        for (String line : data) {
-            System.out.println(line);
-        }
+        Application application = new Application();
+        application.setIslandTools(new IslandTools());
+        application.start();
     }
 }
---
+++ src/main/java/ru/spireworks/bravyi/IslandsInTheSea/IslandTools.java
@@ -0,0 +1,77 @@
+package ru.spireworks.bravyi.IslandsInTheSea;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class IslandTools {
+    private static final Logger LOGGER = Logger.getLogger(IslandTools.class);
+
+    private static final String IS_NOT_NUMBER_ERROR = "Строка, %s не является числом. Построение  карты невозможно!";
+    private static final String ZERO = "0";
+
+    // Получаем все данные из файла построчно
+    public List<String> readFile(final String filePath) {
+        List<String> data = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(new File(filePath));
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = reader.readLine();
+            while (line != null) {
+                data.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    // Получаем все острова с координатами по горизонтали и по вертикали
+    public List<Island> getAllIslands(List<String> coords) {
+        List<Island> islands = new ArrayList<>();
+        Island island = new Island();
+        for (String line : coords) {
+            if (StringUtils.isEmpty(line)) continue;
+            line = StringUtils.deleteWhitespace(line);
+            if(!isDigits(line)) {
+                LOGGER.error(String.format(IS_NOT_NUMBER_ERROR, line));
+                return null;
+            }
+
+            if (checkEndOfLineForZero(line)) {
+                if (island.getHCoords().size() < island.getSize()) {
+                    island.getHCoords().add(line);
+                } else if (island.getVCoords().size() < island.getSize()) {
+                    island.getVCoords().add(line);
+                }
+            } else {
+                if (island.getSize() != 0) islands.add(island);
+                island = new Island();
+                island.setSize(Integer.parseInt(line));
+            }
+        }
+        islands.add(island);
+        return islands;
+    }
+
+    // Проверка, что строка не пустая и заканчивается 0
+    private boolean checkEndOfLineForZero(String line) {
+        return StringUtils.isNotEmpty(line) && line.endsWith(ZERO);
+    }
+
+    // Проверка является ли строка числом
+    private boolean isDigits(String line) {
+        return NumberUtils.isDigits(line);
+    }
+}
