f6c3b4d70f08a78115bc8d48e38e1485cc4ff487
bravyi96 <maksim.bravyi@yandex.ru>
2020-10-03 22:56:10 +0300

    Refactoring

--- src/main/java/ru/spireworks/bravyi/IslandsInTheSea/Application.java
+++ src/main/java/ru/spireworks/bravyi/IslandsInTheSea/Application.java
@@ -2,31 +2,37 @@ package ru.spireworks.bravyi.IslandsInTheSea;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Application {
-    private static final Logger log = Logger.getLogger(Application.class);
     private static final String FILE_PATH = "input_data.txt";
+
     private IslandUtils islandUtils;
 
     private void start() {
         List<String> data = islandUtils.readFile(FILE_PATH);
         List<Island> islands = islandUtils.getAllIslands(data);
 
+        List<String> map = new ArrayList<>();
         for (Island island : islands) {
             System.out.println(island.toString());
-//            List<String> map = new ArrayList<>();
-//
-//            for (int i = 0; i < island.getSize(); i++) {
-//                for (int j = 0 ; j < island.getSize(); j++) {
-//
-//                }
-//            }
+
+            for (int i = 0; i < island.getSize(); i++) {
+                generateMap(map, island.getHCoords().get(i), island.getVCoords().get(i));
+            }
         }
+
+        for (String str : map) {
+            System.out.println(str);
+        }
+    }
+
+    private void generateMap(List<String> map, int[] hor, int[] ver) {
+
     }
 
     public static void main( String[] args ) {
